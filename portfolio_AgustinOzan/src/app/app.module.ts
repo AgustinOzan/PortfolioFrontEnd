0e84bc2b26565ae8511560d51ab8de575adc6038
Agustín Ozán <117683390+AgustinOzan@users.noreply.github.com>
2023-05-02 19:22:36 -0300

    maqueta FrontEnd

--- portfolio_AgustinOzan/src/app/app.module.ts
+++ portfolio_AgustinOzan/src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { AcrcDeComponent } from './components/acrc-de/acrc-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    AcrcDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent,
+    BannerComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ portfolio_AgustinOzan/src/app/components/acrc-de/acrc-de.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acrc-de',
+  templateUrl: './acrc-de.component.html',
+  styleUrls: ['./acrc-de.component.css']
+})
+export class AcrcDeComponent {
+
+}
